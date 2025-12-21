8c81f1672d72fab90741e4ea46e075d35d06c3ed
nguyenpt <nguyenpt@hivelab.com>
2025-12-21 13:33:53 +0700

    update

--- config/email.ts
+++ config/email.ts
@@ -1,14 +1,16 @@
-import nodemailer from "nodemailer";
-import dotenv from "dotenv";
+import nodemailer from "nodemailer"
+import dotenv from "dotenv"
 
 dotenv.config()
 
 var transporter = nodemailer.createTransport({
   service: "gmail",
+  port: 587,
+  secure: false,
   auth: {
     user: "tainguyen.pham.133@gmail.com",
     pass: process.env.EMAIL_PASSWORD || "",
   },
-});
+})
 
 export default transporter
